f11df5cec6ef6523e4b886558fb7fd7d1f33b0b8
arunesh-jain <arunesh.jain@knoldus.com>
2022-06-30 11:16:09 +0530

    First Commit

---
+++ SimpleMockito/src/main/java/com/knoldus/simplemockito/SimplemockitoApplication.java
@@ -0,0 +1,13 @@
+package com.knoldus.simplemockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplemockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplemockitoApplication.class, args);
+	}
+
+}
